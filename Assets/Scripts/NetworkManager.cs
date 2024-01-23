49ac7c44c0a8203b826f5ed201f4984b4b28f3de
Inoksito <51702762+Inoksito@users.noreply.github.com>
2024-01-23 20:03:49 +0100

    Update NetworkManager.cs

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NetworkManager : MonoBehaviour
 {
+    public int mapId;
+    private string mapName;
     // Start is called before the first frame update
     void Start()
     {
