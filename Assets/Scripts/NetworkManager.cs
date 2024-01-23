ed1f809cb94535fc1712d8f84a9a1998d68cf365
CarlosEscuderoQuemada <150896687+CarlosEscuderoQuemada@users.noreply.github.com>
2024-01-23 20:08:00 +0100

    CambioDeVariable
    Añade variables en el script network Manager

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class NetworkManager : MonoBehaviour
 {
+    public string Alias;
+    public string ErrorMessage;
     // Start is called before the first frame update
     void Start()
     {
